ca5afaf9cfa5601ad3de5363f8765427603478ad
sreeram-16 <sreeramsankarasubramanian@gmail.com>
2020-03-27 23:58:09 -0400

    Header file for identifying symptoms.

---
+++ include/disease.h
@@ -0,0 +1,6 @@
+/* This header file has a function definition for "disease" . This function is used to identify
+patient symptoms as chronic or physical and calls another function within it for allocating rooms for patient.
+It takes patient symptoms separated by a demiliter ";" in the file along with patient name and patient phone number.
+Return type is integer. */
+
+int disease(char *symptom, char *patient_name,char*ph);
