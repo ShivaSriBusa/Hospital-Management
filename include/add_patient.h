74eb9d550220f4f16f2633336a12f0ba4a5073dc
sreeram-16 <sreeramsankarasubramanian@gmail.com>
2020-03-27 22:28:38 -0400

    space added in add_patient.h header file

--- include/add_patient.h
+++ include/add_patient.h
@@ -6,14 +6,19 @@ struct patient
 {
     //For storing phone number
     char phoneNo[size], emergencyNo[size];
+
     // For storing date and time of admission and release of patients
     char date_Adm[20], time_Adm[20], date_release[20],time_release[20]; 
+
     //For storing unique patient_identity number (manual input - not auto generated)
     char  patient_identity[100];
+
     //For storing regular Patient details (patient name, address, symptoms, medication, allergies and discharge status)
     char *name, *address, *symp, *medication, *allergies,*status;
+
     //For storing doctors name
     char *doc_name;
+    
 }p1;
 
 /* Function "add_patient" takes input from hospital, details of patients and stores it in "patient_record.csv" */
