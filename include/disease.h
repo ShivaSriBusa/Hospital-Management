31034c190d4b2de31099f48d0c244dd5698a8745
sreeram-16 <sreeramsankarasubramanian@gmail.com>
2020-03-30 15:37:33 -0400

    header file to identify symptoms and allocate room

--- include/disease.h
+++ include/disease.h
@@ -1,6 +1,23 @@
-/* This header file has a function definition for "disease" . This function is used to identify
-patient symptoms as chronic or physical and calls another function within it for allocating rooms for patient.
-It takes patient symptoms separated by a demiliter ";" in the file along with patient name and patient phone number.
-Return type is integer. */
+/**
+ * @file disease.h
+ *
+ * @brief A Function related to the allocation of rooms for admitted patients.
+ *
+ * @author sreeram sankarasubramanian - sreeramsankarasubram@cmail.carleton.ca
+ * 
+ */
+
+/**
+ * @brief A function to allocate room in general ward if patient has physical injury symptoms.
+ * 
+ * @details room capacity is only for 10 patients. Disease function process this signal retuned by rooms function and stores
+ * patient details in "general_rooms.csv" file if symptoms are related to physical injury, If it returns a flag for 
+ * no availability, then appropriate message is displayed. If symptoms are related to chronic, patient details are stored
+ * in "special_rooms.csv". If symptoms are neither physical nor chronic then displays appropriate message.
+ * 
+ * @param[in] It takes patient symptoms separated by a demiliter ";" in the file along with patient name and patient phone number.
+ * 
+ * @return Returns 0 on successful execution.
+ */
 
 int disease(char *symptom, char *patient_name,char*ph);
