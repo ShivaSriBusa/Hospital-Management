855735d9aed643e3cff9dd555a5787b131c99502
jameel204 <jameel204@yahoo.com>
2020-03-29 23:25:58 -0400

    Function "Advice" not completed yet

---
+++ src/Advice.c
@@ -0,0 +1,11 @@
+#pragma warning (disable : 4996)  // disables error-4996 in visual studio
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define size 20
+
+struct patient_advise
+{
+   char *precaution, *diet_advise, *prescription;
+}a1;
+
