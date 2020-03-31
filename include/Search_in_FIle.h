f5fa5483bf00615ab0648183fae18d25663da679
jameel204 <jameel204@yahoo.com>
2020-03-30 23:09:15 -0400

    edited comments in Search_in_FIle.h

--- include/Search_in_FIle.h
+++ include/Search_in_FIle.h
@@ -13,12 +13,14 @@
  * 
  * @details This function reads a file "patient_record.csv" passed as an input, and searches if the 
  * name and phone number of a patient which is also passed as an input to this function  is available 
- * in the given file. If the patient is found, it checks wether the patient is discharged or not. If the
- * patient is already discharged it does nothing and returns back to the main function, if the patient 
- * needs to be discharged,the function updates the "pateint_record.csv" as discharged for the patient and
- * updates the "discharged_list.csv". The "discharged_list.csv" contains list of patients  name who are 
- * discharged with their release time and date. And if the patient is not discharged it allows the user 
- * to input doctor's advice, prescription and precatuions by invoking a separate function "Advice"
+ * in the given file. If the patient is found, it dispalys the patients information by invoking a function 
+ * "display_single_patient". From the displayed information the user can know if the patient is discharged
+ * or not by looking at the discharge status. If the patient is already discharged it does nothing and
+ * returns back to the main function, if the patient needs to be discharged,the function updates the 
+ * "pateint_record.csv" as discharged for the patient and updates the "discharged_list.csv". The 
+ * "discharged_list.csv" contains list of patients  name who are discharged with their release time and 
+ * date. And if the patient is not discharged it allows the user to input doctor's advice, prescription 
+ * and precatuions by invoking a separate function "Advice".
  * 
  * @param[in] It takes file name, patient's name and patient's phone number as input parameter.
  * 
@@ -27,8 +29,8 @@
  */
 
 /* This header file contains the definition for the function "Search_in_File.c". The funtion is used to 
-search a patient by his name and phone numbber and update "patient_record.csv" and "discharged_list.csv"
-if the patient is discharged; or input doctor's advice, precatution and prescriptio if not discharged by
-invoking a separate function "Advice". */
+search a patient by his name and phone number, display the patients information by invoking "display_single
+_patient" and update "patient_record.csv" and "discharged_list.csv if the patient is discharged; or input
+ doctor's advice, precatution and prescriptio if not discharged by invoking a separate function "Advice".*/
 
 int Search_in_File(char *fname, char *str, char*ph);
