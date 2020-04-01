2aacdd74afa6bef9d24f01f950c5ded113848d1d
shivasri543 <shivasri.busa@gmail.com>
2020-04-01 14:23:27 -0400

    welcome message header file added

---
+++ include/welcomeMessage.h
@@ -0,0 +1,18 @@
+/**
+ * @brief Our software is a prototype designed to work only in local server of hospital,
+ *  so it can only be used by one computer at a time.
+ * 
+ * @details Function that prints welcome screen
+ * 
+ * @author Shiva Sri Busa - Shivasribusa@cmail.carleton.ca
+ *
+ * @return Return value is void.
+ *
+ */ 
+
+
+
+
+
+
+void welcomeMessage();
