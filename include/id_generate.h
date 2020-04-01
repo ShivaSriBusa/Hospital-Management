266316ec7927463e1d919cde761a889d7f546f1b
sreeram-16 <sreeramsankarasubramanian@gmail.com>
2020-03-31 23:06:43 -0400

    added id generation header

--- include/id_generate.h
+++ include/id_generate.h
@@ -17,7 +17,5 @@
  * 
  * @return Returns integer, a new row number on successful execution.
  */
-/* This header file has a function definition for "id_generate". This function is used to generate
-new row number for the file which we pass as input. Return type is integer */
 
 int id_generate(char *file);
