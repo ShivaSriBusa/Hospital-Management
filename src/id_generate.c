af7e76912010dff52a7d789a870e0ff8688afb16
sreeram-16 <sreeramsankarasubramanian@gmail.com>
2020-03-31 23:07:04 -0400

    added id generation code file

--- src/id_generate.c
+++ src/id_generate.c
@@ -1,26 +1,47 @@
+/**
+ * @file id_generate.c
+ *
+ * @brief A Function related to the allocation of rooms for admitted patients.
+ *
+ * @author sreeram sankarasubramanian - sreeramsankarasubram@cmail.carleton.ca
+ * 
+ */
+
 #include "include/id_generate.h"
 #include<stdio.h>
 #include<stdlib.h>
 
-/* This function is used to generate new row number for the file which we pass as input. Return type is integer.
-
-Variables used:
-
-fp -> Act as FILE pointer. used for opening a file 
-count -> A flag used for counting number of rows existing in file 
-buff -> used for storing string read from file. 
-
-*/
+/**
+ * @brief A function to generate new row number for file passed as an input.
+ * 
+ * @details This function reads the file passed as an input and counts number of rows in it,
+ * it is then used to generate new row number for the file which we pass as input.
+ * 
+ * @param[in] It takes file name as input parameter.
+ * 
+ * @return Returns integer, a new row number on successful execution.
+ */
 
 int id_generate(char *file)
 {
+    //Act as FILE pointer. used for opening a file 
     FILE *fp = fopen(file,"r");
+
+    //A flag used for counting number of rows existing in file
     int count=0;
+   
+    //used for storing line read from file. 
     char buff[1024];
-	do
+    if (!fp)
+    {
+	   printf("Can't open file\n");
+	   return -1;
+    }
+
+    //Reads line by line from file pointed by fp
+	while(fgets(buff,1024, fp)!= NULL)
 	{
-	 fgets(buff, 1024, (FILE*)fp);
 	 count++;
-	}while((getc(fp))!=EOF);
+	}
     return count;
 }
