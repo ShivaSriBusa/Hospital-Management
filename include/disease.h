fb70fb6a15d6b17a33e2f31ccca2dad1d75e97ee
sreeram-16 <sreeramsankarasubramanian@gmail.com>
2020-03-31 23:03:47 -0400

    added disease function header

--- include/disease.h
+++ include/disease.h
@@ -8,7 +8,8 @@
  */
 
 /**
- * @brief A function to allocate room in general ward if patient has physical injury symptoms.
+ * @brief A function to allocate room in general ward/special ward if patient has physical injury/chronic symptoms 
+ * based on availability. If none of those symptoms then patient is not admitted.
  * 
  * @details room capacity is only for 10 patients. Disease function process this signal retuned by rooms function and stores
  * patient details in "general_rooms.csv" file if symptoms are related to physical injury, If it returns a flag for 
