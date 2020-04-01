5ca01910445d890448efd15e6ea58c2179521f12
jameel204 <jameel204@yahoo.com>
2020-03-31 20:04:40 -0400

    Header file "count_visitor.c" function

---
+++ include/count_visitor.h
@@ -0,0 +1,24 @@
+/**
+ * @file count_visitor.h
+ * 
+ * @brief A function related to counting visitors.
+ * 
+ * @author Kazi Muhammad Jameel - kazimuhammadjameel@cmail.carleton.ca 
+ * 
+ */
+
+/**
+ * @brief A function to count number of visitors from the file 
+ * "visitor_info.c"
+ * 
+ * @details The function reads the "visitor_info.c" file and counts
+ * total number of visitor visited.
+ * 
+ * @param[in] the function takes a filename as input parameter.
+ * 
+ * @return Returns number of visitor as count if the file is read succesfully. 
+ * returns -1 if the file cannot be opened.
+ * 
+ */
+
+int count_visitor(char fname);
