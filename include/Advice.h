c4df1607a9422cbd1c8985ac6c495edb5551b6ba
shivasri543 <shivasri.busa@gmail.com>
2020-04-05 12:33:43 -0400

    updated advice header file

--- include/Advice.h
+++ include/Advice.h
@@ -1,4 +1,4 @@
-/**
+/*
  * @file advice.h
  *
  * @brief A collection of structure and function related to storing advice details in "patient_advice.csv".
@@ -7,7 +7,7 @@
  * 
  */
 
-/**
+/*
  * @brief A Structure containing necessary variables to store the details of advice given to patients.
  * 
  * @details Patient advice information can be stored using variables of structure struct patient_advise.
