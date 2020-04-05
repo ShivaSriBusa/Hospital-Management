f600a6943f4d6c77468ba21023b0c8a5f42708ec
shivasri543 <shivasri.busa@gmail.com>
2020-04-04 21:53:05 -0400

    updated advice.c

--- src/Advice.c
+++ src/Advice.c
@@ -1,4 +1,4 @@
-/**
+﻿/*
  * @file advice.c
  *
  * @brief Implementation of advice details to file.
@@ -8,46 +8,41 @@
  */
 
 
-
-
 #include "include/Advice.h"
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 
-/**
- * @briefHere prescription, precaution and diet advice related to a patient will be displayed. The patient will be searched by his name and number from the �patient_advice.csv� file. 
+/*
+ * @brief Here prescription, precaution and diet advice related to a patient will be displayed.
+ * The patient will be searched by patient name and phone number from the patient_advice.csv file. 
  * Then the corresponding information will be displayed from this file.
  */
 
-int Advice(char *pname,char *ph)
-{
+int advice(char *pname,char *ph){
+    // Integer variable of size 100 for size of memory allocated to pointer variable
+    int SIZE=100,id;
 
-// Integer variable of size 100 for size of memory allocated to pointer variable
-    int s=100,id;
-
-    // file pointer is created to work with files.
+    // File pointer is created to work with files.
     FILE *fptr;
 
-    id=id_generate("patient_advice.csv");
+    //Integer variable id calls the id_generate function to generate row number.
+    id = id_generate("patient_advice.csv");
 
     // Structure variables allocated memory.
-
-    a1.diet_advise = (char *)malloc(s);
-    a1.precaution = (char *)malloc(s);
-    a1.prescription =(char *)malloc(s);
+    a1.diet_advise = (char *)malloc(SIZE);
+    a1.precaution = (char *)malloc(SIZE);
+    a1.prescription = (char *)malloc(SIZE);
        
-    // opening file in append mode.
+    // Opening file in append mode.
     fptr = fopen("patient_advice.csv", "a");
 
-    // exiting program if file is not found.
-    if (fptr == NULL)
-    {
+    //Exiting program if file is not found.
+    if (fptr == NULL){
         printf("Error!");
         exit(1);
     }
-    else
-    {
+    else{
         printf("\nDiet advise for Patient %s\n",pname);
         printf("\nPlease input precaution: ");
         gets(a1.precaution);
@@ -57,11 +52,10 @@ int Advice(char *pname,char *ph)
         gets(a1.diet_advise);
         fprintf(fptr,"%d,%s,%s,%s,%s,%s\n",id,pname,ph,a1.precaution,a1.prescription,a1.diet_advise);
         
-
-     // Closing file read is a good practice.
+	// Closing file read is a good practice.
         fclose(fptr);
 
-     //Freeing the allocated memory to pointer. It is must to free memory after use.
+	//Freeing the allocated memory to pointer. It is must to free memory after use.
         free(a1.diet_advise);
         free(a1.precaution);
         free(a1.prescription);
