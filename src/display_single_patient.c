e41dca085565516023ccc046341768088fc4f3c0
Soummyo <soummyochatto@gmail.com>
2020-04-11 13:55:20 -0400

    displaying a single patient function

--- src/display_single_patient.c
+++ src/display_single_patient.c
@@ -14,14 +14,14 @@
 /** @details Here the user will be able to input the necessary information related to the patient. 
 * Following are the information that will be collected:
 * • Patient id.
-*
+* • Patient Phone number.
 * After taking the patient id, it will check if the patient is admitted in the hospital at that particular moment.
 * If with the patient id a match is found in the "patient_record.csv" file then it will display all the corresponding details of the patient that are stored in the "patient_record.csv" file.
 * 
 *
 * @param[in] Function has two input parameters.
 *
-* @return Function returns -1 if the file "visitor.csv" where the visitor details will be stored is not found. 
+* @return Function prints a message "No record found" if the file "patient_record.csv" does not contain that patient information and returns -1. Else returns 0 on successful execution. 
 */
 
 
