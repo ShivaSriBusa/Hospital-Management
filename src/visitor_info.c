d396cc87290b6698c86f9925c26f652ef8901859
Soummyo <soummyochatto@gmail.com>
2020-04-11 20:31:02 -0400

    displaying the visitor details

--- src/visitor_info.c
+++ src/visitor_info.c
@@ -12,8 +12,8 @@
 
 /** @details Here the user will be able to input the necessary information of the visitor. 
 * Following are the information that will be collected:
-* • Visitor Name.
-* • Visitor Phone number.
+* • Patient Name.
+* • Patient Phone number.
 *
 * After taking the visitor name and phone number as input, it will pass these parameters to add_visitor function to add visitor details if patient is admitted either in general or special ward.
 * If with the visitor details a match is found in either of the files then the visitor details will be displayed.
