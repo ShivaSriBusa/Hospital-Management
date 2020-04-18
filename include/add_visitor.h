b591ef59184d9afaf82dabf5495e2bcf3d4a0ef9
Soummyo <soummyochatto@gmail.com>
2020-04-18 16:22:06 -0400

    adding header file

--- include/add_visitor.h
+++ include/add_visitor.h
@@ -1,5 +1,12 @@
-struct visitor {
-    char phoneNo[11];
+#ifndef size
+#define size 11
+#endif // size
+#ifndef  __add_visitor__
+#define  __add_visitor__
+#endif // __add_visitor__
+struct visitor
+{
+    char phoneNo[size];
     char *name,*address;
 }v1;
 
