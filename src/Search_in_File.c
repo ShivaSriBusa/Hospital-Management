9124c415c000647067019c41340d8f6eb9b3fd16
jameel204 <jameel204@yahoo.com>
2020-04-18 16:41:54 -0400

    intendeations corrected and few minor corrections

--- src/Search_in_File.c
+++ src/Search_in_File.c
@@ -1,5 +1,5 @@
 /**
- * @file Search_in_File.c
+ * @file search_in_File.c
  *
  * @brief Implmentation of updating patient info to a file.
  *
@@ -7,14 +7,15 @@
  */
 
 #pragma warning (disable : 4996)  // disables error-4996 in visual studio
-#include "include/Search_in_File.h"
-#include "include/display_single_patient.h"
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
-#include "include/add_patient.h"
-#include "include/Advice.h"
-#include "include/id_generate.h"
+#include "../include/search_in_file.h"
+#include "../include/display_single_patient.h"
+#include "../include/add_patient.h"
+#include "../include/advice.h"
+#include "../include/id_generate.h"
+#include "../include/read_patient.h"
 
  /** Here the user will be able to update the "Discharge status" in "patient_record.csv" file.
   * To update the "Discharge status" a temporary file "temp.csv" is used.
@@ -38,8 +39,7 @@
   *
   */
 
-int Search_in_File(char* fname, char* str, char* ph)
-{
+int search_in_file(char* fname, char* str, char* ph) {
     // integer variable id  to generate row number by calling id_generate function in "discharged_list.csv". 
     int id;
 
@@ -59,67 +59,68 @@ int Search_in_File(char* fname, char* str, char* ph)
     char string[1024];
 
     // Character pointer token and field.
-    char* token, * field;
+    char *token, *field;
 
     // File pointers to work with files.
-    FILE* fp, * fstream, * fptr2, * fptr1;
+    FILE *fp, *fstream, *fptr2, *fptr1;
 
     // Opening the "discharged_list.csv" in append mode.
-    fstream = fopen("discharged_list.csv", "a");
+    fstream = fopen("../src/discharged_list.csv", "a");
+    printf("\n");
 
     // Open the file "patient_record" passed as input paramenter. If the file is not found returns -1.
-    
-	if((fp = fopen(fname, "r")) == NULL) {
-	return(-1);
+	if((fp = fopen(fname, "r")) == NULL){
+        return(-1);
     }
 
     // If the file "patient_record" is found invoke "display_single_patient" and store return value to flag.
-    flag=display_single_patient(str,ph);
+    flag = display_single_patient(str,ph);
 
     // If flag is -1 function terminates and returns to main.
-    if(flag==-1){
+    if(flag == -1){
         return 0;
     }
 
     // Else check the "Discharge status" of the patient.    
-    else
-    printf("\nIs the patient discharged? (Y/N) :");
-    scanf("%c",&inp);
-    fgetc(stdin);
+    else{
+        printf("\nIs the patient discharged? (Y/N) :");
+        scanf("%c",&inp);
+        fgetc(stdin);
+    }
 
     // If Discharged return to main as patient is already discharged
     if(inp=='Y'){
-    return 0;
+        return 0;
     }
 
-    // Else discharge the patient  or invoke "Advice.c" to enter doctor's advice, prescription and precaution. 
-    else
-    printf("\nDo you want to discharge now? (Y/N) :");
-    scanf("%c",&inp);
-    fgetc(stdin);
+    // Else discharge the patient  or invoke "advice.c" to enter doctor's advice, prescription and precaution. 
+    else{
+        printf("\nDo you want to discharge now? (Y/N) :");
+        scanf("%c",&inp);
+        fgetc(stdin);
+    }
     if(inp=='N'){
-    Advice(str,ph);
+        advice(str,ph);
     }
 
     // If dischrged, enter release time and date and store in "discharged_list.csv" file.  
     // Also update the "patient_record.csv" files "Discharge_status" to "yes".
-    else {
-        id=id_generate("discharged_list.csv");
+    else{
+        id=id_generate("../src/discharged_list.csv");
         printf("\nPlease input date of release (DD-MM-YYYY) : ");
         gets(p1.date_release);
         printf("\nPlease input time of release (HH:MM): ");
         gets(p1.time_release);
         fprintf(fstream,"%d,%s,%s,%s,%s\n",id,str,ph,p1.date_release,p1.time_release);
         fclose(fstream);
-        fptr1=fopen("patient_record.csv","r");
+        fptr1=fopen("../src/patient_record.csv","r");
         if (!fptr1){
             printf("Unable to open the input file!!\n");
             return 0;
         }
-        fptr2=fopen("temp.csv","w");
+        fptr2=fopen("../src/temp.csv","w");
 
         // copy all contents from "patient_record.csv" to the temporary file "temp.csv" other except specific line.
-
         fprintf(fptr2,"ID,Name,Phone Number,Address,Emergency Phone Number,Date of admission (DD-MM-YYYY),");
         fprintf(fptr2,"Time of admission (HH:MM),Symptoms,Doctors name,Identity Number,Medications,Allergies,Discharge status\n");
         while (fgets(string, 1024, fptr1)){
@@ -129,63 +130,61 @@ int Search_in_File(char* fname, char* str, char* ph)
                 continue;
             }
             field = strtok(string, ",");
-            token=field;
+            token = field;
             token = strtok(NULL, ",");
-            if(strcmp(token,str)==0){
+            if(strcmp(token,str) == 0){
                 token = strtok(NULL, ",");
                 if(strcmp(token,ph)==0){
                     id=id_generate("temp.csv");
                     fprintf(fptr2,"%d,",row_count-1);
-                while (field){
-                    if(field_count==0){
-                        fprintf(fptr2,"%s,",str);
-                        field = strtok(NULL, ",");
-                        field_count++;
+                    while (field){
+                        if(field_count==0){
+                            fprintf(fptr2,"%s,",str);
+                            field = strtok(NULL, ",");
+                            field_count++;
+                        }
+                        if(field_count==11){
+                            field=(char *)malloc(100);
+                            fprintf(fptr2,"Yes");
+                            free(field);
+                        }
+                        if(field_count==1){
+                            fprintf(fptr2,"%s,",token);
+                            field_count++;
+                        }
+                        if(field_count!=1){
+                            fprintf(fptr2,"%s,",field);
+                            field = strtok(NULL, ",");
+                            field_count++;
+                        }
                     }
-                    if(field_count==11){
-                        field=(char *)malloc(100);
-                        fprintf(fptr2,"Yes");
-                        free(field);
-                    }
-                    if(field_count==1){
+                }
+            }
+            else{
+                while(field){
+                    if(field_count == 1){
                         fprintf(fptr2,"%s,",token);
                         field_count++;
                     }
-                    if(field_count!=1){
+                    if(field_count != 1){
                         fprintf(fptr2,"%s,",field);
                         field = strtok(NULL, ",");
                         field_count++;
                     }
-                    //printf("%s\n",token);
-                }
+                    if(field_count == 12){
+                        fprintf(fptr2,"%s",field);
+                        field = strtok(NULL, ",");
+                        field_count=0;
+                    }
                 }
             }
-                 else
-                {
-                while(field){
-                if(field_count==1){
-                    fprintf(fptr2,"%s,",token);
-                    field_count++;
-                }
-                if(field_count!=1){
-                    fprintf(fptr2,"%s,",field);
-                    field = strtok(NULL, ",");
-                    field_count++;
-                    }
-                if(field_count==12){
-                    fprintf(fptr2,"%s",field);
-                    field = strtok(NULL, ",");
-                    field_count=0;
-                    }
-                }
-                }
-
         }
         fclose(fptr1);
         fclose(fptr2);
+        
         // Remove the old "patient_record.csv" and rename the "temp.csv" to be the updated "patient_record.csv" file.
-        remove("patient_record.csv");
-        rename("temp.csv","patient_record.csv");
+        remove("../src/patient_record.csv");
+        rename("../src/temp.csv","../src/patient_record.csv");
     }
     return 0;
 }
