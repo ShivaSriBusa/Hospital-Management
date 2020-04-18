a303871c73813024ac9b2991d8506fbd3749b71e
Soummyo <soummyochatto@gmail.com>
2020-04-17 21:15:21 -0400

    Display single patient information function

--- src/display_single_patient.c
+++ src/display_single_patient.c
@@ -6,10 +6,11 @@
  * @author Soummyo Priyo Chattopadhyay - soummyopriyochattopa@cmail.carleton.ca
  * 
  */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "include/display_single_patient.h"
+#include "../include/display_single_patient.h"
 
 /** @details Here the user will be able to input the necessary information related to the patient. 
 * Following are the information that will be collected:
@@ -24,129 +25,121 @@
 * @return Function prints a message "No record found" if the file "patient_record.csv" does not contain that patient information and returns -1. Else returns 0 on successful execution. 
 */
 
-
-
-int display_single_patient(char *name, char *ph)
-{
-    FILE *fp=fopen("patient_record.csv","r");
+int display_single_patient(char *name, char *ph)    {
+    FILE *fp=fopen("../src/patient_record.csv","r");
 	char string[1024];
     int row_count = 0;
-    int field_count = 0;
+    int field_count = 0,t_ph,u_ph,n=0;
     char *field,*token;
-    int n=0;
-     if (!fp) {
+     if (!fp)   {
         printf("Can't open file\n");
-        return 0;
+        return -1;
     }
-    while (fgets(string, 1024, fp))
-        {
-            field_count = 0;
-            row_count++;
-            if (row_count == 1)
-            {
-                continue;
-            }
-            field = strtok(string, ",");
-            token=field;
-            token = strtok(NULL, ",");
-            if(strcmp(token,name)==0)
-            {
-                token = strtok(NULL, ",");
-                if(strcmp(token,ph)==0)
-                {
-                    while (field)
-                    {
-                        n++;
-                        if (field_count == 0) {
-                        printf("Id:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 1) {
-                        printf("Name:");
-                        printf("%s\n",name);
-                        field_count++;
-                        }
-                        if (field_count == 2) {
-                        printf("Phone:");
-                        printf("%s\n",token);
-                        field_count++;
-                        }
-                        if (field_count == 3) {
-                        printf("Address:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 4) {
-                        printf("Emergency phone:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 5) {
-                        printf("Date of Admission:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 6) {
-                        printf("Time of Admission:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 7) {
-                        printf("Symptoms:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 8) {
-                        printf("Doctors name:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 9) {
-                        printf("Identity Number:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 10) {
-                        printf("Medication:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 11) {
-                        printf("Allergies:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                        if (field_count == 12) {
-                        printf("Discharge status:");
-                        printf("%s\n",field);
-                        field = strtok(NULL, ",");
-                        field_count++;
-                        }
-                    }printf("\n");
-                }
-                else
-                {
-                    printf("\nPhone number did not match!");
-                    fclose(fp);
-                    return -1;
+    while (fgets(string, 1024, fp)) {
+    field_count = 0;
+    row_count++;
+    if (row_count == 1) {
+        continue;
+    }
+    field = strtok(string, ",");
+    token=field;
+    token = strtok(NULL, ",");
+    if(strcmp(token,name)==0)   {
+        n++;
+        token = strtok(NULL, ",");
+        t_ph=atoi(token);
+        u_ph=atoi(ph);
+        if(t_ph==u_ph)  {
+            while (field)   {
+
+                if (field_count == 0)   {
+                    printf("Id:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                     field_count++;
+                    }
+                if (field_count == 1)   {
+                    printf("Name:");
+                    printf("%s\n",name);
+                    field_count++;
+                    }
+                if (field_count == 2)   {
+                    printf("Phone:");
+                    printf("%s\n",token);
+                    field_count++;
+                    }
+                if (field_count == 3)   {
+                    printf("Address:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 4)   {
+                    printf("Emergency phone:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 5)   {
+                    printf("Date of Admission:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 6)   {
+                    printf("Time of Admission:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 7)   {
+                    printf("Symptoms:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 8)   {
+                    printf("Doctors name:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 9)   {
+                    printf("Identity Number:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 10)  {
+                    printf("Medication:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 11)  {
+                    printf("Allergies:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+                if (field_count == 12)  {
+                    printf("Discharge status:");
+                    printf("%s\n",field);
+                    field = strtok(NULL, ",");
+                    field_count++;
+                    }
+            }printf("\n");
+        }
+    else    {
+        printf("\nPhone number did not match!");
+        fclose(fp);
+        return -1;
                 }
         }
-        else
-        {
-            printf("\nNo record found!");
-            fclose(fp);
-            return -1;
+    if(n==0)    {
+        printf("\nNo record found!");
+        fclose(fp);
+        return -1;
         }
     }
     fclose(fp);
